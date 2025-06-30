87469a0b5d6d2dddcc975c41515fe9982458bf27
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-06-30 17:33:08 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -8,18 +8,20 @@ class EmailConfig
 {
     static function config($name, $mensaje): PHPMailer
     {
+        $datosemail = General::all();
+
         $mail = new PHPMailer(true);
         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
         $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
+        $mail->Host = $datosemail->smtp;
         $mail->SMTPAuth = true;
-        $mail->Username = 'hola@mundoweb.pe';
-        $mail->Password = 'ejzouhgnohvvbdwa';
+        $mail->Username = $datosemail->emailsend;
+        $mail->Password = $datosemail->contrasend;
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
         $mail->Subject = '' . $name . ', ' . $mensaje;
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom('info@codefinan.com', 'Codefinan S.A.C.');
+        $mail->setFrom($datosemail->emailsend, 'Codefinan S.A.C.');
         return $mail;
     }
 }
